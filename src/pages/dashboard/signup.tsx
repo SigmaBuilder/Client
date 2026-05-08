ccaa28a5038b53ef91c360d24c0bad416739b106
rub3nnn <rubenmoraleszgz@gmail.com>
2026-05-08 17:28:11 +0200

    feat: add signup page with signup form

---
+++ src/pages/dashboard/signup.tsx
@@ -0,0 +1,11 @@
+import { SignupForm } from '@/components/signup-form';
+
+export default function Signup() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-muted/50">
+      <div className="w-full max-w-4xl">
+        <SignupForm />
+      </div>
+    </div>
+  )
+}
