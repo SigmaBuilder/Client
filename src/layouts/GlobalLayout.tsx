623f8d2d3b78580e4be07ad3c9516bf97e345ead
Diego Puertolas <diegopuertolasruiz@gmail.com>
2026-05-09 15:25:35 +0200

    refactor: update ThemeProvider to use custom component and persistent storage key

--- src/layouts/GlobalLayout.tsx
+++ src/layouts/GlobalLayout.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom"
 import { Toaster } from "@/components/ui/sonner"
-import { ThemeProvider } from "next-themes"
+import { ThemeProvider } from "@/components/theme-provider"
 
 export default function GlobalLayout() {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider storageKey="sigma-builder-theme" defaultTheme="system">
       <div className="min-h-screen bg-background font-sans text-foreground antialiased">
         <Outlet />
       </div>
